b38ec43217c8e89ad926bd4e221facdfc921e08c
yasasrangika <rangikaymendis@gmail.com>
2023-04-12 10:48:11 +0530

    complete flow of applying gateway policy

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -6409,4 +6409,9 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
             }
         }
     }
+
+    public void applyGatewayGlobalPolicies(List<GatewayGlobalPolicy> gatewayGlobalPoliciesList, String orgId)
+            throws APIManagementException {
+        apiMgtDAO.addGatewayGlobalPolicy(gatewayGlobalPoliciesList, orgId);
+    }
 }
